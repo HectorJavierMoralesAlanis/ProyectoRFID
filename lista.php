1c214c48b04e1e822063f60b602ba153b3d35cee
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-04 02:38:24 -0600

    Version 11

--- lista.php
+++ lista.php
@@ -1,5 +1,5 @@
 <?php
-    $con = new mysqli("localhost","admin","587e608a4d7c61b1a289769b4f0eed9f2ba5e0edd903e117","listas");
+    $conn = new mysqli("localhost","admin","587e608a4d7c61b1a289769b4f0eed9f2ba5e0edd903e117","listas");
 
     if ($con->connect_error){
         echo "Error: ".$connn->connect_error . PHP_EOL;
@@ -9,7 +9,7 @@
     //$password=$_POST["contra"];
     $query = "SELECT * from lista";
     //echo $query;
-    $lista = $con->query($query);
+    $lista = $conn->query($query);
     //echo "Ejemplo";
     echo $lista('uid');
     if($lista['uid'] === $rfid){
