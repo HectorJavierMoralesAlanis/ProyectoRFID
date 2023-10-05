5df4026f7d7f3fb8ba603b3d57a246fc4d108370
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-05 10:38:59 -0600

    Version 39 con DAO

--- lista.php
+++ lista.php
@@ -14,7 +14,20 @@
     foreach ($alumnosLista as $alumno){
         //echo $rfid;
         //echo $alumno['Contra'];
-        if($alumno['IDcard'] === $rfid || $alumno['Contra'] === $rfid){
+        if($alumno['IDcard'] === $rfid){
+            $dao2 = new DAO();
+            echo "Ingresado";
+            $matricula=$alumno['Matricula'];
+            //echo $matricula;
+            $fecha=date('Y-m-d H:i:s');
+            //echo $fecha;
+            $asistio=1;
+            $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,Fecha)"."VALUES (:matricula,:asistio,:fecha)";
+            $parametros=array("matricula"=>$matricula,"asistio"=>$asistio,"fecha"=>$fecha);
+
+            $pase = $dao2->insertarConsulta($consulta2,$parametros);
+            break;
+        }else if($alumno['Contra']===$rfid){
             $dao2 = new DAO();
             echo "Ingresado";
             $matricula=$alumno['Matricula'];
