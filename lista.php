2df9e8a7494ae6243c72f824b6dad823e85ae69b
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-04 22:38:28 -0600

    Version 15 con DAO

--- lista.php
+++ lista.php
@@ -26,7 +26,7 @@
             $fecha=date('Y-m-d H:i:s');
             $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,No_Asistio,Fecha)"."VALUES (:matricula,:asistio,:no_asistio,:fecha)";
             $parametros=array("matricula"=>$alumno['Matricula'],"asistio"=>1,"no_asistio"=>0,'fecha'=$fecha);
-            $dao2->ejecutarConsulta($consulta2,$parametros);
+            $pase = $dao2->ejecutarConsulta($consulta2,$parametros);
         }else{
             echo "No se encontro la Matricula";
         }
