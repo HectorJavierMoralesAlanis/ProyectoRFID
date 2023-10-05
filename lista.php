9c406d3c56f45d6be298fcc659f9e84730a2d756
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-04 22:43:16 -0600

    Version 18 con DAO

--- lista.php
+++ lista.php
@@ -4,7 +4,7 @@
     $dao = new DAO();
     $consulta1 = "SELECT * FROM Alumnos";
     $alumnosLista = $dao->ejecutarConsulta($consulta1);
-    //$dao2 = new DAO();
+    $dao2 = new DAO();
 
     if ($conn->connect_error){
         echo "Error: ".$connn->connect_error . PHP_EOL;
@@ -26,7 +26,7 @@
             $fecha=date('Y-m-d H:i:s');
             $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,No_Asistio,Fecha)"."VALUES (:matricula,:asistio,:no_asistio,:fecha)";
             $parametros=array("matricula"=>$alumno['Matricula'],"asistio"=>1,"no_asistio"=>0,'fecha'=$fecha);
-            $pase = $dao->ejecutarConsulta($consulta2,$parametros);
+            $pase = $dao2->insertarConsulta($consulta2,$parametros);
         }else{
             echo "No se encontro la Matricula";
         }
