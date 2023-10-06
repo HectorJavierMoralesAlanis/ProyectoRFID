32cb3fb4a77861a378b4e8b13b822651298a133c
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-06 13:48:26 -0600

    Version 10 Pagina

--- materias.php
+++ materias.php
@@ -90,9 +90,9 @@
                                   <!-- EXTRAE TODOS LOS DATOS DE LA TABLA EN LA BASE DE DATOS Y LOS MUESTRA AQUI -->
                                   <?php foreach ($clases as $clase) { ?>
                                   <tr>
-                                      <!--<td><?php echo $clase['ID']; ?></td>
+                                      <td><?php echo $clase['ID']; ?></td>
                                       <td><?php echo $clase['Materia']; ?></td>
-                                      <td class="align-middle"><a href="./dahs.php?id=<?php echo $clase['matriculaMaestro']?>" method="POST" class="btn btn-info btn-block btn-sm">Ingresar</a></td>-->
+                                      <td class="align-middle"><a href="./dahs.php?id=<?php echo $clase['matriculaMaestro']?>" method="POST" class="btn btn-info btn-block btn-sm">Ingresar</a></td>
                                   </tr>
                                   <?php }?>
                               </table>
