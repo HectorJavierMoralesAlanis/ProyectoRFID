742cb27aa33de6d9ace53f29faeab6137ff522f6
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-07 22:31:36 -0600

    Version 17 Pagina

--- materias.php
+++ materias.php
@@ -14,50 +14,7 @@
   <title>Materias</title>
 </head>
 <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-  <!--Navbar-->
-  <nav class="main-header navbar navbar-expand navbar-dark">
-      <ul class="navbar-nav">
-          <li class="nav-item">
-              <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-          </li>
-          <li class="nav-item d-none d-sm-inline-block">
-              <a href="../dashboard.php?id=<?php echo $_GET['id']?>" class="nav-link">Inventario</a>
-          </li>
-      </ul>
-  </nav>
 
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-      <!-- Logo --->
-      <a href="../dashboard.php?id=<?php echo $_GET['id']?>" class="brand-link">
-          <img src="./lemur.png"  class="brand-image img-circle elevation-3" style="opacity: .8">
-          <span class="brand-text font-weight-light">Admin</span>
-      </a>
-      <!-- Sidebar -->
-      <div class="sidebar">
-          <!-- Menu lateral-->
-          <nav class="mt-2">
-              <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                      <li class="nav-item">
-                          <a href="../../login.php" class="nav-link">
-                              <p>
-                                  Cerrar sesion
-                              </p>
-                          </a>
-                      </li>
-              </ul>
-          </nav>
-      </div>
-  </aside>
-  <!-- Fin del menu lateral -->
-  <div class="content-wrapper">
-      <div class="content-header">
-          <div class="container-fluid">
-              <div class="row mb-2">
-                  <div class="col-sm-6">
-                  </div>
-              </div>
-          </div>
-      </div>
   <!-- Contenido -->
       <section class="content">
           <div class="container-fluid">
