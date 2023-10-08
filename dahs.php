73395a94da4a4a09b844a547a88cea1fc548bfc3
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-08 17:05:40 -0600

    Version 63 Pagina

--- dahs.php
+++ dahs.php
@@ -90,12 +90,7 @@
   var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
+      labels: ['Alumnos'],
     },
     options: {
       scales: {
