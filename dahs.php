422eb65c02105db15ea384ac58cf1b486c986126
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-08 13:42:13 -0600

    Version 43 Pagina

--- dahs.php
+++ dahs.php
@@ -62,5 +62,28 @@
       </section>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
+<script>
+    (function () {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+})
+</script>
 </body>
 </html>
