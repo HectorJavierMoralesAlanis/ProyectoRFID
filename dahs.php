6ceb79bd9c0d819d07cc0850e2daa81a2c2fa2a0
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-08 16:40:30 -0600

    Version 61 Pagina

--- dahs.php
+++ dahs.php
@@ -77,7 +77,7 @@
                           <h1>Gráfico de Ventas por Mes</h1>
                         <div class="grafico">
                             <?php foreach ($datos as $dato): ?>
-                                <div class="barra" style="width: 15px;">
+                                <div class="hrafico" style="width: 15px;">
                                     <?php $dato ?>
                                 </div>
                             <?php endforeach; ?>
