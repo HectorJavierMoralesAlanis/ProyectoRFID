c7f731a9a012d99b5449e11946b69dcc31da55b5
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-08 18:13:03 -0600

    Version 80 Pagina

--- dahs.php
+++ dahs.php
@@ -22,12 +22,12 @@
     }
 
     foreach($datos as $matricula){
-        /*foreach($alumnos as $al){
+        foreach($alumnos as $al){
             while(in_array($matricula,$al)){
                 $asistencia[$matricula]=$asistencia+1;
             }
         }
-        echo $asistencia[$matricula];*/
+        //echo $asistencia[$matricula];*/
         echo $matricula;
         echo 'jhkbed';
     }
