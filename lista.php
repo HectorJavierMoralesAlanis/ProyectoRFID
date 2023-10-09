943942332f0d276d4355e4de21a0fb0138f518c5
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-08 21:13:11 -0600

    Version 128 Pagina

--- lista.php
+++ lista.php
@@ -21,29 +21,43 @@
         */
         if($alumno['IDcard'] === $rfid){
             $dao2 = new DAO();
+            $dao3 = new DAO();
+            $dao4 = new DAO();
             echo "Ingresado";
             $matricula=$alumno['Matricula'];
-            //echo $matricula;
+            $consulta3 = "SELECT clase FROM Alumnos WHERE Matricula =: matricula";
+            $sentencia3 = array("matricula"=>$matricula);
+            $clase = $dao4->ejecutarConsulta($consulta3,$parametros3);
+            $consulta4 = "SELECT grupo FROM Alumnos WHERE Matricula =: matricula";
+            $sentencia4 = array("matricula"=>$matricula);
+            $grupo = $dao4->ejecutarConsulta($consulta4,$parametros4); 
+            //echo $matricula;                  
             $fecha=date('Y-m-d H:i:s');
             //echo $fecha;
             $asistio=1;
-            $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,Fecha)"."VALUES (:matricula,:asistio,:fecha)";
-            $parametros=array("matricula"=>$matricula,"asistio"=>$asistio,"fecha"=>$fecha);
+            $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,Fecha,grupo,clase)"."VALUES (:matricula,:asistio,:fecha,:grupo,:clase)";
+            $parametros=array("matricula"=>$matricula,"asistio"=>$asistio,"fecha"=>$fecha,"grupo"=>$grupo,"clase"=>$clase);
             $pase = $dao2->insertarConsulta($consulta2,$parametros);
             break;
             //Se cambio la forma de verificar
         }else if($alumno['Contra']===$rfid){
             $dao2 = new DAO();
+            $dao3 = new DAO();
+            $dao4 = new DAO();
             echo "Ingresado";
-            
             $matricula=$alumno['Matricula'];
-            //echo $matricula;
+            $consulta3 = "SELECT clase FROM Alumnos WHERE Matricula =: matricula";
+            $sentencia3 = array("matricula"=>$matricula);
+            $clase = $dao4->ejecutarConsulta($consulta3,$parametros3);
+            $consulta4 = "SELECT grupo FROM Alumnos WHERE Matricula =: matricula";
+            $sentencia4 = array("matricula"=>$matricula);
+            $grupo = $dao4->ejecutarConsulta($consulta4,$parametros4); 
+            //echo $matricula;                  
             $fecha=date('Y-m-d H:i:s');
             //echo $fecha;
             $asistio=1;
-            $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,Fecha)"."VALUES (:matricula,:asistio,:fecha)";
-            $parametros=array("matricula"=>$matricula,"asistio"=>$asistio,"fecha"=>$fecha);
-
+            $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,Fecha,grupo,clase)"."VALUES (:matricula,:asistio,:fecha,:grupo,:clase)";
+            $parametros=array("matricula"=>$matricula,"asistio"=>$asistio,"fecha"=>$fecha,"grupo"=>$grupo,"clase"=>$clase);
             $pase = $dao2->insertarConsulta($consulta2,$parametros);
             break;
         }else{
