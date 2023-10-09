0a3dbb69e67cd43eef40a4b71d33ed83df2b3566
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-08 22:00:35 -0600

    Version 137 Pagina

--- datos.php
+++ datos.php
@@ -2,11 +2,12 @@
     include('DAO.php');
     $dao3 = new DAO();
     $dao4 = new DAO();
-    $matricula=2030103;
+    $matricula=20301031;
     //echo $matricula;
     $consulta3="SELECT clase FROM Alumnos WHERE Matricula=:matricula";
     $parametros3=array("matricula"=>$matricula);
     $claseArreglo=$dao3->insertarConsulta($consulta3,$parametros3);
+    echo "Esto esto es directo.\n";
     echo $claseArreglo;
     $consulta4="SELECT grupo FROM Alumnos WHERE Matricula=:matricula";
     $parametros4=array("matricula"=>$matricula);
