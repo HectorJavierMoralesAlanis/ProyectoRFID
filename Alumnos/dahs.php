11f0d9901eb592bee516377570b56a04ec3af581
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-10 03:39:11 -0600

    Version 193 Pagina

--- Alumnos/dahs.php
+++ Alumnos/dahs.php
@@ -5,52 +5,6 @@
     $consulta = "SELECT * FROM Pase_de_lista Where Matricula=:id";
     $parametros = array("id"=>$_GET['matricula']);
     $alumnos = $dao->ejecutarConsulta($consulta,$parametros);
-/*
-    $datos = [];
-    $asistencia=[];
-    $aux=0;
-    /*
-    //Funcion para contar asistencia
-    function asistencia_Matricula($matricula, $dao) {
-        $consulta = "SELECT COUNT(*) AS asistencias FROM Pase_de_lista WHERE Matricula = :matricula AND Asistio = 1";
-        $parametros = array("matricula" => $matricula);
-        $asistencias = $dao->ejecutarConsulta($consulta, $parametros);
-    
-        return $asistencias[0]['asistencias'];
-    }
-    $x=0;
-    $asistencias = [];
-    foreach ($alumnos as $alumno) {
-        if(in_array($alumno['Matricula'],$asistencias)){
-
-        }else{
-            $asistencias[$x] = asistencia_Matricula($alumno['Matricula'], $dao);
-        }
-        $x=$x+1;
-    }
-    
-    $matriculas = [];
-    foreach ($alumnos as $alumno) {
-        if (!in_array($alumno['Matricula'], $matriculas)) {
-            $matriculas[] = $alumno['Matricula'];
-        }
-    }
-    function porcentaje_Asistencia($matricula, $dao) {
-        $asistencias = asistencia_Matricula($matricula, $dao);
-        $total_clases = 5;
-        $porcentaje = $asistencias / $total_clases * 100;
-    
-        return $porcentaje;
-    }
-    
-    $matriculas = [];
-    $porcentajes = [];
-    foreach ($alumnos as $alumno) {
-        if (!in_array($alumno['Matricula'], $matriculas)) {
-            $matriculas[] = $alumno['Matricula'];
-            $porcentajes[] = porcentaje_Asistencia($alumno['Matricula'], $dao);
-        }
-    } */
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -120,14 +74,7 @@
                                   <?php }?>
                               </table>
                           </div>
-                          <h1>Gráfico de Ventas por Mes</h1>
-                        <div class="container">
-                            <canvas id="myChart" width="400" height="400"></canvas>
-                        </div>
-                        <h1>Grafico porcentaje</h1>
-                        <div class="container">
-                            <canvas id="myChart2" width="400" height="400"></canvas>
-                        </div>
+
                       </div>
                   </div>
               </div>
@@ -135,65 +82,4 @@
       </section>
 </div>
 </body>
-<script>
-    const alumnos = <?php echo json_encode($asistencias);?>;
-    const nMatriculas = <?php echo json_encode($matriculas);?>;
-    const asistenciaPorcentaje = <?php echo json_encode($porcentajes);?>;
-    console.log(alumnos);
-    function getRandomColor() {
-        const colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff"];
-        return colors[Math.floor(Math.random() * colors.length)];
-    }
-    var ctx = document.getElementById("myChart").getContext("2d");
-    var ctx2 = document.getElementById("myChart2").getContext("2d");
-
-    const asistenciaAlumnos = {
-        label: "Asistencia",
-        data: alumnos.map(alumno => alumno),
-        backgroundColor: 'rgba(237,78,136, 0.2)', // Color de fondo
-        borderColor: 'rgba(237,78,136, 1)', // Color del borde
-        borderWidth: 1, // Ancho del borde
-    };
-    const asistenciaPor = {
-        label: "Porcentaje",
-        data: asistenciaPorcentaje.map(asisP => asisP),
-        backgroundColor: asistenciaPorcentaje.map(asisP => getRandomColor()), // Color de fondo aleatorio para cada sector
-        borderColor: "rgba(0,0,0, 1)", // Color del borde
-        borderWidth: 1, // Ancho del borde
-    };
-    var myChart = new Chart(ctx, {
-        type: 'line', // Tipo de gráfica
-        data: {
-            labels: nMatriculas.map(nmat => nmat),
-            datasets: [
-                asistenciaAlumnos,
-                // Aquí más datos...
-            ]
-        },
-        options: {
-        scales: {
-            y: {
-            beginAtZero: true
-            }
-        }
-        }
-    });
-
-
-    // Creamos un nuevo gráfico
-    var myChart = new Chart(ctx2, {
-        type: "pie", // Tipo de gráfica circular
-        data: {
-            labels: nMatriculas.map(nmat => nmat),
-            datasets: [asistenciaPor],
-        },
-        options: {
-            scales: {
-            y: {
-                beginAtZero: true,
-            },
-            },
-        },
-    });
-</script>
 </html>
