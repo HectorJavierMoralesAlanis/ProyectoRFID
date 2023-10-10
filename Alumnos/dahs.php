1dccf49378af0dc78521793fa4823f004be2412a
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-10 03:23:26 -0600

    Version 186 Pagina

--- Alumnos/dahs.php
+++ Alumnos/dahs.php
@@ -9,7 +9,6 @@
 
     foreach($alumnos as $alumno){
         $clase = $alumno['clase'];
-    
         $consultaClase = "SELECT * FROM Clases Where id=:clase";
         $parametrosClase = array("clase"=>$clase);
         $clasesArr = $daoMateria->ejecutarConsulta($consultaClase,$parametrosClase);
