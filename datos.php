c91689c5d2bad4e0babd5ea38ba9de2507bc8cf5
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-11 03:47:21 -0600

    Version 224 Pagina

--- datos.php
+++ datos.php
@@ -25,7 +25,7 @@
             echo $horas['hora'];
             echo " hora final ";
             echo $horas['hora_final'];
-            if($horas['hora']==$hora){
+            if($horas['hora']>=$hora){
                 echo "SI";
                 $grupo=$horas['grupo'];
                 $clase=$horas['id'];
