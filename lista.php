df42d0e55fa800a8413a534c87ebe6f66f312d95
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-11 02:55:49 -0600

    Version 198 Pagina

--- lista.php
+++ lista.php
@@ -21,7 +21,7 @@
         echo "\nContra base ";
         echo $alumno['Contra'];
         */
-        if($alumno['IDcard'] === $rfid || $alumno['IDcard']===$rfid){
+        if($alumno['IDcard'] === $rfid){
             $dao2 = new DAO();
             $dao3 = new DAO();
             $dao4 = new DAO();
@@ -76,13 +76,43 @@
             }
             
             $asistio=1;
-            $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,Fecha,grupo,clase)"."VALUES (:matricula,:asistio,:fecha,:grupo,:clase)";
-            $parametros=array("matricula"=>$matricula,"asistio"=>$asistio,"fecha"=>$fecha,"grupo"=>$grupo,"clase"=>$clase);
+            $consulta2="INSERT INTO Pase_de_lista (Matricula,Asistio,Fecha,hora,grupo,clase)"."VALUES (:matricula,:asistio,:fecha,:hora,:grupo,:clase)";
+            $parametros=array("matricula"=>$matricula,"asistio"=>$asistio,"fecha"=>$fecha,"hora"=>$hora,"grupo"=>$grupo,"clase"=>$clase);
             $pase = $dao2->ejecutarConsulta($consulta2,$parametros);
             break;
         }else{
-            $x=$x+1;
-            //if ( $x ===count($alumnosListas)){
+            
+            foreach($maestroLista as $maestro){
+                if($maestro['IDcard']===$rfid){
+                    //Se crean los dao
+                    $dao2Maestro = new DAO();
+                    $daoHora = new DAO();
+                    //Se obtiene la matricula del maestro
+                    $matricula=$maestro['Matricula'];
+                    // Se establecen la hora
+                    date_default_timezone_set('America/Monterrey');
+                    $fecha=date('Y-m-d');
+                    $hora=date('07:00:00');
+                    //Se obtiene la hora de la materia para designar el grupo del maestro
+                    $consultaHora = "SELECT * FROM Clases Where matriculaMaestro=:matricula";
+                    $parametrosHora = array("matricula"=>$matricula);
+                    $resultadoHora = $daoHora->ejecutarConsulta($consultaHora,$parametrosHora);
+                    foreach($resultadoHora as $horas){
+                        if($horas['hora']>=$hora && $horas['hora_final']<=$hora){
+                            $grupo=$horas['grupo'];
+                            $clase=$horas['id'];
+                        }
+                    }
+                    $asistio=1;
+                    $consultaPase = "INSERT INTO Pase_de_lista (Matricula,Asistio,Fecha,hora,grupo,clase)"."VALUES (:matricula,:asistio,:fecha,:hora,:grupo,:clase)";
+                    $parametrosPase = array("matricula"=>$matricula,"asistio"=>$asistio,"fecha"=>$fecha,"hora"=>$hora,"grupo"=>$grupo,"clase"=>$clase);
+                    $paseMaestro = $dao2Maestro->ejecutarConsulta($consultaPase,$parametrosPase);
+                    break;
+                }else{
+                    $x=$x+1;
+                }
+                break;
+            }//if ( $x ===count($alumnosListas)){
             //    echo "No se encontro la Matricula";
             //}
         }
