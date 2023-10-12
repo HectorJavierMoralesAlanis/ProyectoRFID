329f4e3c4042202b1c0ff87c59762ea6dde5f6d0
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-12 02:21:55 -0600

    Version 273 Pagina

--- dahs.php
+++ dahs.php
@@ -158,7 +158,6 @@
                                         <td class="align-middle"><a href="./borrar.php?id=<?php echo $alumno['id']?>&clase=<?php echo $alumno['clase']?>" method="POST" class="btn btn-info btn-block btn-sm">Eliminar</a></td>
                                         <?php $asistencias[$alumno['Matricula']]=$dias;
                                             if(in_array($alumno['Matricula'],$asistenciasMatriculas)){
-
                                             }else {$asistenciasMatriculas=$alumno['Matricula'];}?>
                                         <?php }?>
                                         </tr>
