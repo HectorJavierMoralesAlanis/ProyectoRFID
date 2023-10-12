ebfaeda92af64b494b2af78cc6c53542369a4de2
HectorJavierMoralesAlanis <2030103@upv.edu.mx>
2023-10-12 04:10:42 -0600

    Version 315 Pagina

--- dahs.php
+++ dahs.php
@@ -169,9 +169,9 @@
                                         </tr>
                                     <?php }?>
                                 <?php //}?>
-                                <?php print_r($matriculasAsistidas)?>
+                                <?php //print_r($matriculasAsistidas)?>
                                 <?php print_r ($asistenciasTabla)?>
-                                
+
                                 <?php /*echo $asistenciasTabla['2030103'][2]*/?>
                                     <?php //echo "Pasa";?>
                                     <?php if($semana[$i] === "Viernes"){ ?>
